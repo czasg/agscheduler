02d42f810ea7106a73e5eafdec6eac2b6e6aad02
陈子昂 <chenziang@antiy.cn>
2021-01-22 17:57:45 +0800

    test: add meta

--- job.go
+++ job.go
@@ -46,19 +46,20 @@ type ITask interface {
 }
 
 type Job struct {
-	tableName      struct{}      `json:"-" pg:"ags_jobs"`
-	Id             int           `json:"id" pg:",pk"`
-	Name           string        `json:"name" pg:",use_zero,unique"`
-	Task           ITask         `json:"-" pg:"-"`
-	Trigger        ITrigger      `json:"-" pg:"-"`
-	Status         STATUS        `json:"status" pg:",use_zero"`
-	NotCoalesce    bool          `json:"not_coalesce" pg:",use_zero"`
-	MaxInstances   int           `json:"max_instances" pg:",use_zero"`
-	DelayGraceTime time.Duration `json:"delay_grace_time" pg:",use_zero"`
-	/* should be init by AGS. */
-	Scheduler   AGScheduler   `json:"-" pg:"-"`
-	NextRunTime time.Time     `json:"next_run_time" pg:",use_zero"`
-	Logger      *logrus.Entry `json:"-" pg:"-"`
+	tableName      struct{}               `json:"-" pg:"ags_jobs"`
+	Id             int                    `json:"id" pg:",pk"`
+	Name           string                 `json:"name" pg:",use_zero,unique"`
+	Task           ITask                  `json:"-" pg:"-"`
+	Trigger        ITrigger               `json:"-" pg:"-"`
+	Status         STATUS                 `json:"status" pg:",use_zero"`
+	NotCoalesce    bool                   `json:"not_coalesce" pg:",use_zero"`
+	MaxInstances   int                    `json:"max_instances" pg:",use_zero"`
+	DelayGraceTime time.Duration          `json:"delay_grace_time" pg:",use_zero"`
+	Scheduler      AGScheduler            `json:"-" pg:"-"`
+	NextRunTime    time.Time              `json:"next_run_time" pg:",use_zero"`
+	Logger         *logrus.Entry          `json:"-" pg:"-"`
+	TriggerMeta    TriggerMeta            `json:"trigger_meta" pg:",use_zero"`
+	TaskMeta       map[string]interface{} `json:"task_meta" pg:",use_zero"`
 }
 
 func (j *Job) FillByDefault() {
--- trigger.go
+++ trigger.go
@@ -14,3 +14,12 @@ var (
 type ITrigger interface {
 	GetNextRunTime(previous, now time.Time) time.Time
 }
+
+type TriggerMeta struct {
+	Type         string        `json:"type"`
+	NextRunTime  time.Time     `json:"next_run_time"`
+	Interval     time.Duration `json:"interval"`
+	StartRunTime time.Time     `json:"start_run_time"`
+	EndRunTime   time.Time     `json:"end_run_time"`
+	CronCmd      string        `json:"cron_cmd"`
+}
