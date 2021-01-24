56cc67b8e683f285e79f6677405b0f772dccf371
lemon-lisa <2130076416@qq.com>
2021-01-24 21:55:19 +0800

    feat: can't add support

--- task.go
+++ task.go
@@ -2,12 +2,15 @@ package agscheduler
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"reflect"
 	"sync"
 )
 
 var (
-	allITasks = map[string]ITask{}
+	allITasks = map[string]reflect.Type{}
 	taskLock  = sync.Mutex{}
 )
 
@@ -15,8 +18,82 @@ type ITask interface {
 	Run(ctx context.Context)
 }
 
+func addTask(taskName string, taskType reflect.Type) {
+	defer taskLock.Unlock()
+	taskLock.Lock()
+	_, ok := allITasks[taskName]
+	if !ok {
+		allITasks[taskName] = taskType
+	}
+}
+
+func SerializeTask(job *Job) error {
+	if job.Task == nil {
+		return errors.New("task is nil.")
+	}
+	if job.TaskMeta == nil {
+		job.TaskMeta = map[string]interface{}{}
+	}
+	body, err := json.Marshal(job.Task)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &job.TaskMeta)
+	if err != nil {
+		return err
+	}
+	taskT := reflect.TypeOf(job.Task)
+	if taskT.Kind() == reflect.Ptr {
+		taskT = taskT.Elem()
+	}
+	job.TaskMeta[""] = taskT.String()
+	return nil
+}
+
+func DeserializeTask(job *Job) error {
+	if job.TaskMeta == nil {
+		return errors.New("task meta is nil.")
+	}
+	taskNameI, ok := job.TaskMeta[""]
+	if !ok {
+		return errors.New("task mate not define task name.")
+	}
+	taskName, ok := taskNameI.(string)
+	if !ok {
+		return errors.New("task name is not string.")
+	}
+	taskT, ok := allITasks[taskName]
+	if !ok {
+		return errors.New("task is not type of reflect.Type.")
+	}
+	fmt.Println(taskT)
+	newTaskT := reflect.New(taskT)
+	if newTaskT.Kind() == reflect.Ptr {
+		newTaskT = newTaskT.Elem()
+	}
+	for i := 0; i < taskT.NumField(); i++ {
+		fieldName := taskT.Field(i).Name
+		fieldValue, ok := job.TaskMeta[fieldName]
+		if !ok {
+			return errors.New("task's field value is not define.")
+		}
+		newTaskT.Field(i).Set(reflect.ValueOf(fieldValue))
+	}
+	fmt.Println(newTaskT, allITasks)
+	newTask, ok := newTaskT.Interface().(ITask)
+	if !ok {
+		return fmt.Errorf("task[%v] is not type of ITask.", newTaskT.String())
+	}
+	job.Task = newTask
+	return nil
+}
+
 func RegisterAllTasks(tasks ...ITask) {
 	for _, task := range tasks {
-		reflect.TypeOf(task)
+		taskT := reflect.TypeOf(task)
+		if taskT.Kind() == reflect.Ptr {
+			taskT = taskT.Elem()
+		}
+		addTask(taskT.String(), taskT)
 	}
 }
---
+++ task_test.go
@@ -0,0 +1,152 @@
+package agscheduler
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type TestTask struct {
+	Name string
+	Age  float64
+}
+
+func (t *TestTask) Run(ctx context.Context) {
+	fmt.Println(t.Name, t.Age)
+}
+
+func TestDeserializeTask(t *testing.T) {
+	job := Job{
+		Name: "test",
+		Task: &TestTask{
+			Name: "test-task",
+			Age:  18,
+		},
+	}
+	RegisterAllTasks(&TestTask{})
+	err := SerializeTask(&job)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(job.Task)
+	job.Task = nil
+	fmt.Println(job.Task)
+	type args struct {
+		job *Job
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "pass",
+			args: args{
+				job: &job,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := DeserializeTask(tt.args.job); (err != nil) != tt.wantErr {
+				t.Errorf("DeserializeTask() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			fmt.Println(job.Task)
+			//err := SerializeTask(&job)
+			//if err != nil {
+			//	panic(err)
+			//}
+			//fmt.Println(job.Task)
+		})
+	}
+}
+
+func TestRegisterAllTasks(t *testing.T) {
+	type args struct {
+		tasks []ITask
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "pass",
+			args: args{
+				[]ITask{&TestTask{}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			RegisterAllTasks(tt.args.tasks...)
+			if len(allITasks) == 0 {
+				t.Errorf("tasks should not be null.")
+			}
+		})
+	}
+}
+
+func TestSerializeTask(t *testing.T) {
+	RegisterAllTasks(&TestTask{})
+	type args struct {
+		job *Job
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "pass",
+			args: args{
+				job: &Job{
+					Name: "test",
+					Task: &TestTask{
+						Name: "test-task",
+						Age:  18,
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SerializeTask(tt.args.job); (err != nil) != tt.wantErr {
+				t.Errorf("SerializeTask() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.args.job.TaskMeta != nil {
+				t.Errorf("task meta should not be nil.")
+			}
+		})
+	}
+}
+
+func Test_addTask(t *testing.T) {
+	type args struct {
+		taskName string
+		taskType reflect.Type
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "pass",
+			args: args{
+				taskName: "test",
+				taskType: reflect.TypeOf("test"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			addTask(tt.args.taskName, tt.args.taskType)
+			if allITasks["test"] == nil {
+				t.Errorf("tasks should not't be nil.")
+			}
+		})
+	}
+}
